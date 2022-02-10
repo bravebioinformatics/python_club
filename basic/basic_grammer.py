18cc5705cb65939e7df6aa66b09cfd8955fe3b54
bravebioinformatics <info@brave-bioinformatics.com>
2022-02-10 15:26:37 +0900

    added homework 20220210

--- basic/basic_grammer.py
+++ basic/basic_grammer.py
@@ -26,5 +26,27 @@ print(c)
 print(b + c)
 
 #I would like you to add b + c. How can you do that? Write your answer below. 
+#Mika's solution was to convert variable into string using str(). Fantastic!!
+
+#20220210
+#let's define a list
+age = [25, 30, 18, 52, 68, 41, 55, 24, 19, 36, 40, 62]
+
+#Print the length of the list.
+
+#let's define another list
+sex = ['man','woman','woman','woman','man','woman','man','man','man','woman','man','woman']
+
+#In the list sex, it would be a little messy to have to handle strings all the time. Can you think of any ways to make handling of the list easier?
+#hint: the list only has two values(!)
+
+#Now let's pair up those two lists and make a dictionary. Set "age" as the key. 
+
+#Is the person who is 24 years old man or woman? Please leave the code, not just the answer. 
+
+#Can you add one more patient information to the dictionary? 
+# 18 years old, man
+
+
 
 
